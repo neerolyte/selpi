e6a7d9db3fc9678bdaf76f12dcce0ac42207eac6
David Schoen <dave@lyte.id.au>
2021-02-20 14:24:11 +1100

    Improve dump performance

--- commands/dump.py
+++ commands/dump.py
@@ -12,12 +12,18 @@ def run(args):
 
     # Before 0xa001 we don't get anything back
     start=0xa001
-    # How much to get at a time (also tied to console line length currently)
-    length=0x10
+    # How much to get at a time
+    fetch_words=0x100
+    line_words=0x10
     # When to stop
-    end=0xa2ff
+    end=0xa301
     print('          1   2   3   4   5   6   7   8   9   a   b   c   d   e   f   0')
-    for address in range(start, end, length):
-        hexaddy = binascii.hexlify(struct.pack(">H", address))
-        mem = binascii.hexlify(protocol.query(Range(address, length)))
-        print(b''.join([b'0x',hexaddy,b' ',mem]).decode('utf-8'))
+    for address in range(start, end, fetch_words):
+        memory = protocol.query(Range(address, fetch_words))
+        for offset in range(0, fetch_words, line_words):
+            start_bytes = offset * 2
+            end_bytes = (offset + line_words) * 2
+            sub_memory = memory[start_bytes:end_bytes]
+            hex_memory = binascii.hexlify(sub_memory)
+            hex_address = binascii.hexlify(struct.pack(">H", address + offset))
+            print(b''.join([b'0x',hex_address,b' ',hex_memory]).decode('utf-8'))
